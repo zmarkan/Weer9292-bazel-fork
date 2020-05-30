99f31fa32f81f8e9f05fa064adc8e26d9632fb7a
Patrick Kuijpers <patrickuijpers@gmail.com>
2020-05-30 21:48:46 +0200

    Create 3 basic screens for each feature

--- app/src/main/java/nl/tcilegnar/weer9292/ui/forecast/ForecastViewModel.kt
+++ app/src/main/java/nl/tcilegnar/weer9292/ui/forecast/ForecastViewModel.kt
@@ -1,13 +1,13 @@
-package nl.tcilegnar.weer9292.ui.notifications
+package nl.tcilegnar.weer9292.ui.forecast
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ForecastViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is forecast Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/nl/tcilegnar/weer9292/ui/radar/RadarViewModel.kt
+++ app/src/main/java/nl/tcilegnar/weer9292/ui/radar/RadarViewModel.kt
@@ -1,13 +1,13 @@
-package nl.tcilegnar.weer9292.ui.dashboard
+package nl.tcilegnar.weer9292.ui.radar
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class RadarViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Weather radar feature coming soon!"
     }
     val text: LiveData<String> = _text
 }
