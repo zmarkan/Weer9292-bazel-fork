7e5ea64ce46320349d3b5729635d25cd0160f031
Patrick Kuijpers <patrickuijpers@gmail.com>
2020-06-01 14:45:54 +0200

    Fix crash due to incorrect observed lifeCycleOwner

--- app/src/main/java/nl/tcilegnar/weer9292/ui/forecast/ForecastFragment.kt
+++ app/src/main/java/nl/tcilegnar/weer9292/ui/forecast/ForecastFragment.kt
@@ -24,7 +24,7 @@ class ForecastFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        forecastViewModel.forecast.observe(this, Observer { forecast ->
+        forecastViewModel.forecast.observe(viewLifecycleOwner, Observer { forecast ->
             forecast?.let {
                 text_forecast.text = it.weatherResponses[0].epoch.toString()
             }
