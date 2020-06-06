c3904cbf702571d92daea2e1b1aea27985ecae99
Patrick Kuijpers <patrickuijpers@gmail.com>
2020-06-06 12:18:09 +0200

    Details fragment should also observe C/F temperature unit settings

--- app/src/main/java/nl/tcilegnar/weer9292/ui/weatherdetail/WeatherDetailsFragment.kt
+++ app/src/main/java/nl/tcilegnar/weer9292/ui/weatherdetail/WeatherDetailsFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_weather_details.*
 import nl.tcilegnar.weer9292.R
+import nl.tcilegnar.weer9292.model.Temperatures
 import nl.tcilegnar.weer9292.model.WeatherDetails
+import nl.tcilegnar.weer9292.storage.TemperaturePrefs
 
 class WeatherDetailsFragment : Fragment() {
-
     private val args: WeatherDetailsFragmentArgs by navArgs()
+
+    private lateinit var temperaturePrefs: TemperaturePrefs
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        temperaturePrefs = TemperaturePrefs(requireContext())
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_weather_details, container, false)
@@ -29,10 +38,6 @@ class WeatherDetailsFragment : Fragment() {
         weather_detail_location.setValue(weather.location.getCityWithCountryCode())
         weather_detail_coordinates.setValue(weather.location.coordinates)
         weather_detail_description.setValue(weatherDetails.weatherDescription)
-        weather_detail_temp_current.setTemperature(weather.temperatures.currentTemperature)
-        weather_detail_temp_feel.setTemperature(weather.temperatures.temperatureFeelsLike)
-        weather_detail_temp_max.setTemperature(weather.temperatures.temperatureMax)
-        weather_detail_temp_min.setTemperature(weather.temperatures.temperatureMin)
         weather_detail_wind_speed.setValue(weather.wind.speed)
         weather_detail_wind_angle.setValue(weather.wind.degrees)
         weather_detail_wind_gust.setValue(weather.wind.gust)
@@ -40,5 +45,15 @@ class WeatherDetailsFragment : Fragment() {
         weather_detail_humidity.setValue(weatherDetails.humidity)
         weather_detail_sunrise.setValue(weatherDetails.getSunriseTimeFormatted())
         weather_detail_sunset.setValue(weatherDetails.getSunsetTimeFormatted())
+        temperaturePrefs.getTemperatureUnitLiveDataString().observe(viewLifecycleOwner, Observer {
+            setTemperatures(weather.temperatures)
+        })
+    }
+
+    private fun setTemperatures(temperatures: Temperatures) {
+        weather_detail_temp_current.setTemperature(temperatures.currentTemperature)
+        weather_detail_temp_feel.setTemperature(temperatures.temperatureFeelsLike)
+        weather_detail_temp_max.setTemperature(temperatures.temperatureMax)
+        weather_detail_temp_min.setTemperature(temperatures.temperatureMin)
     }
 }
